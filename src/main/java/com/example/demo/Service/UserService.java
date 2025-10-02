d755c20a522583a19cd4854d69520631efad956c
kimtr <nagneoo@naver.com>
2025-10-02 15:54:57 +0900

    password 암호

--- src/main/java/com/example/demo/Service/UserService.java
+++ src/main/java/com/example/demo/Service/UserService.java
@@ -1,6 +1,6 @@
 package com.example.demo.Service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,7 +17,7 @@ public class UserService {
 //	@Autowired
     private final UserRepository userRepository;
 	//@Autowired
-   // private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public void registerUser(UserDTO request) {
@@ -29,14 +29,20 @@ public class UserService {
             throw new IllegalArgumentException("이미 사용중인 이메일입니다.");
         }
 
-        // 2. 비밀번호 암호화
-     //   String encodedPassword = passwordEncoder.encode(request.getPassword());
+        String encodedPassword=null;
+        if(request.getPassword().length() >= 8) {
+        	  // 2. 비밀번호 암호화
+            encodedPassword = passwordEncoder.encode(request.getPassword());
+        }else {
+        	throw new IllegalArgumentException("비밀번호 8자리 이상입니다.");
+        }
+      
 
         // 3. 영속화 전 상태  user entity만든 상태 
         User user = new User();
         user.setName(request.getName());
         user.setEmail(request.getEmail());
-        user.setPassword(request.getPassword());
+        user.setPassword(encodedPassword);
         // 영속화 컨텍스트에 저장 
         userRepository.save(user);
         // 이제 데이터베이스는 영속화 컨텍스트에 의해 관리
